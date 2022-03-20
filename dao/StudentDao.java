821b4ad2c1e5e4dde494f64f82d40a9a034938a8
yfh914127 <309314434@qq.com>
2022-03-20 10:32:02 +0800

    11-bug修复-功能完善

--- dao/StudentDao.java
+++ dao/StudentDao.java
@@ -3,7 +3,7 @@ package com.itheima.edu.info.manager.dao;
 import com.itheima.edu.info.manager.domain.Student;
 
 public class StudentDao {
-    private Student[] students=new Student[5];
+    private static Student[] students=new Student[5];
     public boolean addStudent(Student student) {
         //1.创建学生对象数组
         //2.添加学生到数组
